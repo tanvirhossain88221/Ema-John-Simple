bcbd9c7402ab0719add4390da130b760dcdd5a29
Tanvir Hossain Robin <tanvirhossain88221@gmail.com>
2021-03-09 13:02:08 +0600

    routing done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
@@ -6,7 +8,7 @@ const Cart = (props) => {
     let total = 0;
     for(let i = 0; i< cart.length; i++){
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
     }
 
     let shipping = 0;
@@ -36,6 +38,12 @@ const Cart = (props) => {
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax & Vat: {formatNumber(tax)}</small></p>
             <p>Total Price: {grandTotal}</p>
+            <br/>
+            {/* <Link to="/review"><button className="main-button">Review Order</button></Link> */}
+            {/* <NavLink><button className="main-button">Review Order</button></NavLink> */}
+            {
+                props.children
+            }
         </div>
     );
 };
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Sorry.page not Found</h1>
+            <h1>404! ERROR</h1>
+        </div>
+    );
+};
+
+export default NotFound;
